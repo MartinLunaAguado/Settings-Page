d0213e49777d50e974dadad2f1b7b0d749a4d1d3
Martin Luna Aguado <m.laguado@alumnos.upm.es>
2025-05-04 17:53:05 +0200

    3 commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
+import { Tab, TabKey } from '@/components/types';
+import { ref, computed} from 'vue';
+import TabLink from '@/components/TabLink.vue';
+import GeneralSettings from '@/components/GeneralSettings.vue';
+ import NotificationsSettings from '@/components/NotificationsSettings.vue';
+ import PrivacySettings from '@/components/PrivacySettings.vue';
 
 
 
-</script>
+const tabs: Tab[] = [
+  { key: 'Gneral',   label: 'General',
+  component: GeneralSettings },
+  { key: 'Notifications', label: 'Notifications',
+  component: NotificationsSettings },
+  { key: 'Privacy',  label: 'Privacy',
+  component: PrivacySettings }
+];
+
+const currentTab = ref<TabKey>('General');
+const currentTabComponent = computed(() => tabs.find(tab => tab.key === currentTab.value)?.component);
+</script>                       
 
 <template>
+  <main class="max-w-2xl mx-auto px-4">
+    <nav class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4">
+      <ul class="flex flex-wrap -mb-px">
+        <li v-for="tab in tabs" :key="tab.key">
+          <TabLink :tab="tab" :currentTab="currentTab" @click="currentTab = tab.key" />
+        </li>
+      </ul>
+    </nav>
+    <component :is="currentTabComponent" />
 
 
-  <main>
-    Hello World
   </main>
 </template>
---
+++ src/components/types.ts
@@ -0,0 +1,26 @@
+
+import type { Component } from 'vue';
+ 
+export type TabKey = 'General' | 'Notifications' | 'Privacy';
+
+
+
+
+
+
+export interface Tab {
+ key: TabKey;
+ label: string;
+ component: Component;
+
+
+}
+
+
+
+
+
+
+
+
+
